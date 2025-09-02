842ee235122b059876853995708bb0948d80daf2
Jaedyn <Jaedyn@Jaedyn_Laptop.unl.edu>
2025-09-01 22:55:14 -0500

    added name and major

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,8 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
 		System.out.println("My major is Computer Science.");
+		System.out.println("My name is Esha Vadde.");
+		System.out.println("My major is Computer Science!");
 
-		
 	}
 }
